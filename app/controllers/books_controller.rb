40c62eb9befb5311ba3d195064ff6b0df603d3e7
EC2 Default User <ec2-user@ip-172-31-43-93.ap-northeast-1.compute.internal>
2024-01-02 06:36:04 +0000

    Bookers2-debugいいね機能2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,10 +9,11 @@ class BooksController < ApplicationController
   def index
     @book = Book.new
     @books = Book.all
+    # @book = Book.find(params[:id])
   end
 
   def create
-    @book = Book.new(book_params)
+    @booker = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
       redirect_to book_path(@book), notice: "You have created book successfully."
